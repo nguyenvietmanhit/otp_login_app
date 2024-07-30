9761b45ddff90c715fa162b5eab284847914db59
Kevin Pham <kevin.pham1@vmogroup.com>
2024-07-30 16:20:59 +0700

    a

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,8 +7,8 @@
 <h2>Login</h2>
 <form method="POST" action="{{ route('otp.send') }}">
     @csrf
-    <label for="email">Email:</label>
-    <input type="email" name="email" required>
+    <label for="phone">Phone:</label>
+    <input type="phone" name="phone" required>
     <button type="submit">Send OTP</button>
 </form>
 @if ($errors->any())
